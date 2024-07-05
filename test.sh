879291841cb1be6f07ddd64f9103e8846cc57b77
Erjon Thaqi <e3cthe@e3ag.net>
2024-07-05 07:55:49 +0200

    proof of concept with test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo ello
+DIR="$1"
+declare -A FILE_MOD_TIMES
+
+for FILE in "$DIR"/*; do
+  [ -f "$FILE" ] && FILE_MOD_TIMES["$FILE"]=$(stat -c %Y "$FILE")
+done
+
+while true; do
+  sleep 1
+  for FILE in "$DIR"/*; do
+    if [ -f "$FILE" ]; then
+      NEW_MODIFIED_TIME=$(stat -c %Y "$FILE")
+      
+      if [ "$NEW_MODIFIED_TIME" != "${FILE_MOD_TIMES["$FILE"]}" ]; then
+        # here is command to save to new location  
+        FILE_MOD_TIMES["$FILE"]="$NEW_MODIFIED_TIME"
+      fi
+    fi
+  done
+done
