75f9fe1ae87154342c4a39ec68a30bc8dd9a37b1
Erjon Thaqi <e3cthe@e3ag.net>
2024-07-05 08:57:18 +0200

    loops through fil in dir

--- test.sh
+++ test.sh
@@ -3,20 +3,31 @@
 DIR="$1"
 declare -A FILE_MOD_TIMES
 
-for FILE in "$DIR"/*; do
-  [ -f "$FILE" ] && FILE_MOD_TIMES["$FILE"]=$(stat -c %Y "$FILE")
-done
+function readFilesFromDir(){
+    cd "$DIR"
+    LIST=`ls -p | grep -v /`        
+    observeFilesForChanges $LIST
+}
 
-while true; do
-  sleep 1
-  for FILE in "$DIR"/*; do
-    if [ -f "$FILE" ]; then
-      NEW_MODIFIED_TIME=$(stat -c %Y "$FILE")
-      
-      if [ "$NEW_MODIFIED_TIME" != "${FILE_MOD_TIMES["$FILE"]}" ]; then
-        # here is command to save to new location  
-        FILE_MOD_TIMES["$FILE"]="$NEW_MODIFIED_TIME"
-      fi
-    fi
-  done
-done
+function observeFilesForChanges(){
+    for FILE in $@; do
+        echo $FILE
+    done
+} 
+
+function checkFileForChanges(){
+    LAST=`stat "$1"`
+
+    while true; do { 
+            LAST=`stat "$1"`
+            sleep 1
+            NEW=`stat "$1"`
+            if [ "$NEW" != "$LAST" ]; then
+                echo a file has been cahnged
+                LAST="$NEW"
+            fi
+        }; 
+    done
+} 
+
+readFilesFromDir 
