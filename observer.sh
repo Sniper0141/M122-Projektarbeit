d5cc39914ad73816c1df6470be6001e76be8ea40
Erjon Thaqi <e3cthe@e3ag.net>
2024-07-16 15:51:13 +0200

    tests if backup dir exists and creates if not

--- observer.sh
+++ observer.sh
@@ -7,7 +7,10 @@ while true; do
     NEW=`stat "$1"`
     if [ "$NEW" != "$LAST" ]; then
         source_file="${1}"   
-        destination_dir="${3}/${2}/${1}"    
+        destination_dir="${3}/${2}/${1}"
+        # wenn man auch versionieren möchte kann man anfangen die 
+        # änderungs daten im filenamen zu hinterlegen aber unser script  
+        # ist nur ein backup tool für den fall das die daten verloren gehen       
         cp "$source_file" "$destination_dir"
         LAST="$NEW"
     fi
